0e60377bfe2576ceecc9bf53037b13b85129fc48
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-03-23 22:07:11 -0300

    Abstract functions for GenericFragment

--- app/src/main/java/com/marcelocuevas/moodtracker/GenericFragment.kt
+++ app/src/main/java/com/marcelocuevas/moodtracker/GenericFragment.kt
@@ -1,8 +1,25 @@
 package com.marcelocuevas.moodtracker
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 abstract class GenericFragment: Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layout(), container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    abstract fun layout(): Int
+
+    abstract fun init()
 }
--- app/src/main/java/com/marcelocuevas/moodtracker/SettingsFragment.kt
+++ app/src/main/java/com/marcelocuevas/moodtracker/SettingsFragment.kt
@@ -2,5 +2,13 @@ package com.marcelocuevas.moodtracker
 
 class SettingsFragment: GenericFragment() {
 
+    override fun layout(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
 }
